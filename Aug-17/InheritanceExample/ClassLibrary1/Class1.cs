b6d6be705f219807d73a06b309080d3e71ad3d16
Harsha <harshaglobal@gmail.com>
2020-08-17 09:33:44 +0530

    Aug 17

---
+++ Aug-17/InheritanceExample/ClassLibrary1/Class1.cs
@@ -0,0 +1,24 @@
+﻿public class Person
+{
+    private string PersonName { get; set; }
+
+    //parent class's constructor that initializes only parent class's fields
+    public Person(string PersonName)
+    {
+        this.PersonName = PersonName;
+    }
+}
+public class Employee : Person
+{
+    public int EmpID { get; set; }
+
+    public void Method2()
+    {
+        base.Method1();
+    }
+}
+public class Student : Person
+{
+    public int rollNo { get; set; }
+}
+
