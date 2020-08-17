191af189da3cffef7288d25d9467a1087f5ee039
Harsha <harshaglobal@gmail.com>
2020-08-17 11:04:36 +0530

    Aug 17

--- Aug-17/InheritanceExample/ClassLibrary1/Class1.cs
+++ Aug-17/InheritanceExample/ClassLibrary1/Class1.cs
@@ -1,24 +1,64 @@
 ﻿public class Person
 {
-    private string PersonName { get; set; }
+    private string _personName;
+
+    public string PersonName
+    {
+        get
+        {
+            return _personName;
+        }
+        set
+        { 
+            if (value.Length <= 30)
+            {
+                _personName = value;
+            }
+        }
+    }
 
     //parent class's constructor that initializes only parent class's fields
     public Person(string PersonName)
     {
         this.PersonName = PersonName;
     }
+
+    //parent class's method
+    public virtual void Display(int x)
+    {
+        System.Console.WriteLine("Person name: " + PersonName);
+    }
 }
 public class Employee : Person
 {
     public int EmpID { get; set; }
 
-    public void Method2()
+    public Employee(string empName, int empID) : base(empName)
     {
-        base.Method1();
+        this.EmpID = empID;
+    }
+
+    //child class's method
+    public override void Display(int x)
+    {
+        base.Display(x); //calls Person.Display
+        System.Console.WriteLine("Emp ID: " + EmpID);
     }
 }
-public class Student : Person
-{
-    public int rollNo { get; set; }
-}
+//public class Student : Person
+//{
+//    public int rollNo { get; set; }
+//}
 
+public class ContractBasedEmployee : Employee
+{
+    public ContractBasedEmployee(string empName, int empID) : base(empName, empID)
+    {
+    }
+
+    public override void Display(int x)
+    {
+        base.Display(x); //calls Employee.Display
+        System.Console.WriteLine("Other output here");
+    }
+}
--- Aug-17/InheritanceExample/InheritanceExample/Program.cs
+++ Aug-17/InheritanceExample/InheritanceExample/Program.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace InheritanceExample
+﻿class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        //read inputs
+        System.Console.WriteLine("Enter person name:");
+        string s = System.Console.ReadLine();
+
+        System.Console.WriteLine("Enter Emp ID:");
+        int n = int.Parse(System.Console.ReadLine());
+
+        //create object of child class (ContractBasedEmployee)
+        ContractBasedEmployee emp = new ContractBasedEmployee(s, n);
+
+        //call the method from the child class's object
+        emp.Display(100); //Output: Person Name: xxxxx, Emp ID: xxxxxxxxxx
+
+        System.Console.ReadKey();
     }
 }
+
