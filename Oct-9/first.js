47077d206a2ccd946bd177fb7e2f58587a7f4c2d
Harsha Vardhan <harshaglobal@gmail.com>
2020-10-09 10:33:05 +0530

    oct 9

---
+++ Oct-9/first.js
@@ -0,0 +1,26 @@
+console.log("Welcome to JS");
+
+//declare variables
+var a = 100;
+var b = "Hello";
+var c = 'Hai';
+var d = true;
+var e = false;
+var f = null; //blank
+var g = function() {}; //function
+var h = {}; //object
+const noOfMonthsOfYear = 12;
+
+//print values to console
+console.log(a); //Output: 100
+console.log(b); //Output: Hello
+console.log(c); //Output: Hai
+console.log(d); //Output: true
+console.log(e); //Output: false
+console.log(f); //Output: null
+console.log(g); //Output: Function() {}
+console.log(h); //Output: Object { }
+console.log(noOfMonthsOfYear); //Output: 12
+
+//noOfMonthsOfYear = 13;
+//console.log(noOfMonthsOfYear); //Output: 13
---
+++ Oct-9/operators.js
@@ -0,0 +1,75 @@
+//Arithemtical Operators
+var a = 10;
+var b = 3;
+var c = a + b; //addition
+var d = a - b; //subtraction
+var e = a * b; //multiplication
+var f = a / b; //division
+var g = a % b; //remainder (modulo)
+console.log(c); //Output: 13
+console.log(d); //Output: 7
+console.log(e); //Output: 30
+console.log(f); //Output: 3.33333333333
+console.log(g); //Output: 1
+
+//Assignment Operators
+a = 10; //assigns to
+console.log(a); //Output: 10
+a += 3; //add and assign    a = a + 3
+console.log(a); //Output: 13
+a -= 3; //subtract and assign    a = a - 3
+console.log(a); //Output: 10
+a *= 3; //multiply and assign    a = a * 3
+console.log(a); //Output: 30
+a /= 3; //multiply and assign    a = a / 3
+console.log(a); //Output: 10
+a %= 3; //remainder assigns    a = a / 3
+console.log(a); //Output: 1
+
+
+//Increment / Decrement Operators
+a = 10;
+console.log(a++); //Post incrementation....Output: 10  and internally 'a' value becomes 11
+console.log(a); //Output: 11
+console.log(a--); //Post decrementation....Output: 11  and internally 'a' value becomes 10
+console.log(a); //Output: 10
+console.log(++a); //Pre incrementation....Output: 11
+console.log(a); //Output: 11
+console.log(--a); //Pre decrementation....Output: 10
+console.log(a); //Output: 10
+
+
+//Relational Operators
+a = 10;
+b = 3;
+console.log(a == b); //Output: false
+console.log(a != b); //Output: true
+console.log(a < b); //Output: false
+console.log(a <= b); //Output: false
+console.log(a > b); //Output: true
+console.log(a >= b); //Output: true
+
+
+//Logical Operators
+a = 10;
+b = 3;
+c = 20;
+d = (a > b) && (c > b); //Both conditions should be true
+console.log(d); //true
+d = (a > b) && (c < b); //Both conditions should be true
+console.log(d); //false
+e = (a > b) || (c < b); //At least any one condition should be true
+console.log(e); //true
+
+
+//Concatenation Operator
+a = "Hello";
+b = "World";
+c = a + b; //Attaches the second operand at the end of first operand
+console.log(c); //Output: HelloWorld
+
+//Template Literals
+var name = "Scott",
+    age = 20;
+var message = `Hello ${name} - your age is ${age}`;
+console.log(message); //Output: Hello Scott - your age is 20
