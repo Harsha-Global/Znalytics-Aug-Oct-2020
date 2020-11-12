dba23303853ba54df1ffbaeac168c7dc13f30f1b
Harsha Vardhan <harshaglobal@gmail.com>
2020-11-12 09:02:40 +0530

    Nov 12

---
+++ Nov-12/ProductsWebAPI/ProductsWebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using ProductsWebAPI.Models;
+
+namespace ProductsWebAPI.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class ProductsController : ApiController
+    {
+        public List<Product> Get()
+        {
+            List<Product> products = new List<Product>() { 
+                new Product() { id = 1, productName = "Bluetooth Head phones", price = 2300, quantity = 0 },
+                new Product() { id = 2, productName = "iPhone 12", price = 44750, quantity = 0 },
+                new Product() { id = 3, productName = "Lenovo Keyboard", price = 780, quantity = 0 },
+                new Product() { id = 4, productName = "Dell Monitor", price = 12450, quantity = 0 },
+                new Product() { id = 5, productName = "Sony Camera", price = 22450, quantity = 0 }
+            };
+
+            return products;
+        }
+    }
+}
---
+++ Nov-12/ProductsWebAPI/ProductsWebAPI/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductsWebAPI.Models
+{
+    /// <summary>
+    /// Defines structure of Product model
+    /// </summary>
+    public class Product
+    {
+        public int id { get; set; }
+        public string productName { get; set; }
+        public double price { get; set; }
+        public int quantity { get; set; } = 0;
+    }
+}
+
