a1149cb51c8e1ec5db0abdddf6d375926d9ef384
Harsha Vardhan <harshaglobal@gmail.com>
2020-11-16 12:49:06 +0530

    Nov 16

--- Nov-16/grid-demo/src/app/products-component/products.component.ts
+++ Nov-16/grid-demo/src/app/products-component/products.component.ts
@@ -18,11 +18,7 @@ export class ProductsComponent {
     this.productsService.getProducts().subscribe(
       //handleData starts
       (response: Product[]) => {
-        //less than 0        :   a comes before b
-        //greater than 0  :   b comes before a
-        //equal to 0         :   a and b are left unchanged
-
-        this.sort(response);
+        this.products = response;
       },
       //handleData ends
 
@@ -31,40 +27,4 @@ export class ProductsComponent {
       }
     );
   }
-
-  sort(response: Product[]) {
-    response.sort((a, b) => {
-      //checking whether both arguments are truthy values: not null, not undefined, not 0, not ""
-      if (a[this.sortBy] && b[this.sortBy]) {
-        //store values to compare in two variables
-        let firstValue = a[this.sortBy];
-        let secondValue = b[this.sortBy];
-
-        //checking data type of column value
-        if (typeof firstValue == 'string' && typeof secondValue == 'string') {
-          firstValue = firstValue[0].toString().toLowerCase();
-          secondValue = secondValue[0].toString().toLowerCase();
-        }
-
-        if (firstValue < secondValue) {
-          return -1;
-        } else if (firstValue > secondValue) {
-          return 1;
-        } else {
-          return 0;
-        }
-      } else {
-        return 0;
-      }
-    });
-    //end of sort() method
-
-    if (this.sortOrder == 'DESC') {
-      response = response.reverse();
-    }
-
-    console.log(response); //sorted
-
-    this.products = response;
-  } //end of sort
 } //end of class
--- Nov-16/grid-demo/src/app/products.module.ts
+++ Nov-16/grid-demo/src/app/products.module.ts
@@ -2,26 +2,21 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SortPipe } from './pipes/sort.pipe';
 import { ProductsComponent } from './products-component/products.component';
 import { ProductsRoutingModule } from './products-routing.module';
 
 @NgModule({
-  declarations: [
-    ProductsComponent
-  ],
+  declarations: [ProductsComponent, SortPipe],
 
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
   ],
 
-  exports: [
-    ProductsComponent
-  ]
+  exports: [ProductsComponent],
 })
-export class ProductsModule
-{
-}
+export class ProductsModule {}
